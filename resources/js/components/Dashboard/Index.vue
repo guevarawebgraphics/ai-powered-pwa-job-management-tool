ba473f38dd772f9c7d30e7424d5cdd85b1bdae2f
King Richard Guevara <42199746+kingrgdev@users.noreply.github.com>
2025-04-04 02:55:29 +0800

    fixes

--- resources/js/components/Dashboard/Index.vue
+++ resources/js/components/Dashboard/Index.vue
@@ -84,12 +84,12 @@
                     class="bg-white rounded-[12px] shadow-[rgba(100,100,111,0.2)_0px_7px_29px_0px] border p-4 flex flex-col items-start transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95 focus:ring-2 focus:ring-gray-300"
                 >
                     <div class="flex items-center space-x-2">
-                        <i
+                        <i v-if="this.latestNotif.featured_content"
                             class="fas fa-thumbs-up text-lg text-[#171A1FFF]"
                         ></i>
                         <span
                             class="text-sm text-left font-medium text-[#666666FF]"
-                            >{{ this.latestNotif.featured_content }}</span
+                            >{{ this.latestNotif.featured_content ?? 'No Latest Notification' }}</span
                         >
                     </div>
                     <!-- <p class="text-sm text-gray-500">New Job Request Dryer, no Heat, Stuart</p> -->
